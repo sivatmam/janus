1abd61335956cda36967fdfbbe6fa40d7417ab1a
Sivatma Maharaj <sivatma.maharaj@gmail.com>
2022-04-01 13:25:02 -0400

    Fourth Commit to see if I can push from my laptop

---
+++ teletact/ja004/SyncEnvs.java
@@ -0,0 +1,9 @@
+package teletact.ja004;
+
+public class SyncEnvs{
+
+  public static void main(String[] args){
+    System.out.println("This is just a simple test of syncing my java development environments.");
+    System.out.println("I'll see if I can push from here.");
+  }
+}
