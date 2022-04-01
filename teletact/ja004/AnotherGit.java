b0807b2ef5732ab4eb88aa7cff42267cc8939bac
Sivatma Maharaj <sivatma.maharaj@gmail.com>
2022-04-01 13:50:52 -0400

    Modifying an existing file.  Making changes to AnotherGit.

--- teletact/ja004/AnotherGit.java
+++ teletact/ja004/AnotherGit.java
@@ -4,5 +4,7 @@ public class AnotherGit{
     public static void main(String[] args){
         System.out.println("This is another step in setting up a git environment.");
         System.out.println("Hopefully this works.");
+
+        System.out.println("This is a modification to this class.  Let's see how to synch this to github.");
     }
 }
