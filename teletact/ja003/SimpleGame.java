df004c1f9374a367e293a74e828f0dcee9bed0db
Sivatma Maharaj <siv_maharaj@hotmail.com>
2022-04-01 13:10:58 -0400

    Adding a third project

---
+++ teletact/ja003/SimpleGame.java
@@ -0,0 +1,10 @@
+package teletact.ja003;
+
+public class SimpleGame{
+
+  public static void main(String[] args){
+
+    System.out.println("You entered this first: "+args[0]);
+    System.out.println("You entered this second: "+args[1]);
+  }
+}
