6881cdd1b92b3997e6ea180ec530f38ceab6fdf1
Sivatma Maharaj <siv_maharaj@hotmail.com>
2022-04-03 13:15:37 -0400

    Making changes to Record examples on zeus

--- teletact/ja005/StateTest.java
+++ teletact/ja005/StateTest.java
@@ -5,9 +5,10 @@ public class StateTest {
     public static void main(String[] args) {
         State NY = new State("New York", "Albany", "New York City", "Rochester", "Buffalo", "Syracuse");
         State OH = new State("Ohio", "Columbus", "Cleveland", "Cincinatti", "Dayton", "Toledo");
-
+        State IN = new State("Indiana", "Indianopolis");
         System.out.println(NY);
         System.out.println(OH);
+	System.out.println(IN);
     }
     
 }
